ff2ff345c524c30d6f39c0d2da90d290f76528a3
Comple <comple@complecwaft.com>
2026-07-25 13:44:08 -0700

    Fixed a few styling issues and some reactive stuff

--- The Landing Page/Components/NavBarContainer.cs
+++ The Landing Page/Components/NavBarContainer.cs
@@ -1,7 +1,49 @@
-﻿namespace the_landing_page.Components
+﻿using Microsoft.AspNetCore.Components;
+
+namespace the_landing_page.Components
 {
+    public struct NavBarItem
+    {
+        public required string path { get; set; }
+
+        public required string description { get; set; }
+        public string? icoPath { get; set; } // These have fallbacks if unset
+        public string? name { get; set; }
+        public bool localPath { get; set; }
+        public bool enabled { get; set; }
+
+        public NavBarItem()
+        {
+            this.path = "/"; // Whoopsies!
+            this.localPath = false;
+            this.enabled = true;
+        }
+    }
     public class NavBarContainer
     {
+        public NavBarItem[] navigations = new NavBarItem[] {
+            new NavBarItem()
+            {
+                path = "git",
+                description = "<p>Local Repositories for various projects from the team</p>",
+                name = "Git",
+                enabled = false,
+            },
+            new NavBarItem()
+            {
+                path = "pelican",
+                description = "<p>Use Pelican to manage the various types of game servers available here</p>",
+                name = "Pelican",
+            },
+            new NavBarItem()
+            {
+                path = "seerr",
+                description = "<p>Check what media is new right now, and make requests to see if it can be acquired for you.</p><br /><p>Not public.</p>",
+                name = "Seerr",
+            },
+
+            };
+
         private bool _hidden = true;
 
         public bool navBarHidden
