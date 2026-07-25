8f725e432b9fa07be843aee3998d463abc93df72
Comple <comple@complecwaft.com>
2026-07-25 10:59:38 -0700

    Initial Commit

---
+++ The Landing Page/Components/NavBarContainer.cs
@@ -0,0 +1,27 @@
+﻿namespace the_landing_page.Components
+{
+    public class NavBarContainer
+    {
+        private bool _hidden = true;
+
+        public bool navBarHidden
+        {
+            get => _hidden;
+            set
+            {
+                _hidden = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void toggleBar()
+        {
+            navBarHidden = !navBarHidden;
+        }
+    }
+}
+
